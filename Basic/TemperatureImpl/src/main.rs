6a59e81112af058d1c1fda4a033108aec2dfcef8
Martin1088 <martin.jurk@dkfz-heidelberg.de>
2023-07-04 11:37:49 +0000

    print

--- Basic/TemperatureImpl/src/main.rs
+++ Basic/TemperatureImpl/src/main.rs
@@ -3,11 +3,15 @@ struct Temperature {
     degree_c: f64,
 }
 impl Temperature{
-    fn show_temp(temp: &Temperature) {
-        print!("{} degree F", temp.degree_f);
-        print!("{} degree C", temp.degree_c);
+    fn show_temp(&self) {
+        print!("{} degree F", self.degree_f);
+        print!("{} degree C", self.degree_c);
     }
 }
 fn main() {
-
+    let hot = Temperature{
+        degree_f : 99.9,
+        degree_c : 0.00,
+    };
+    hot.show_temp();
 }
