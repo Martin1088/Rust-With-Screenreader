351693aa7fe9be04efb6f21ff3fefb0b3ab3609e
Martin1088 <martin.jurk@dkfz-heidelberg.de>
2023-07-05 10:51:49 +0000

    emum

--- Basic/TemperatureImpl/src/main.rs
+++ Basic/TemperatureImpl/src/main.rs
@@ -3,15 +3,24 @@ struct Temperature {
     degree_c: f64,
 }
 impl Temperature{
+    fn freezing() -> Self {
+        Self {
+            degree_f: 32.0,
+            degree_c: 0.0,
+        }
+    }
     fn show_temp(&self) {
-        print!("{} degree F", self.degree_f);
-        print!("{} degree C", self.degree_c);
+        println!("{} degree F", self.degree_f);
+        println!("{} degree C", self.degree_c);
     }
 }
 fn main() {
     let hot = Temperature{
         degree_f : 99.9,
-        degree_c : 0.00,
-    };
+        degree_c : 37.2,
+           };
     hot.show_temp();
+    let cold = Temperature::freezing();
+    cold.show_temp();
+
 }
--- Exercise/src/bin/a12.rs
+++ Exercise/src/bin/a12.rs
@@ -9,5 +9,32 @@
 // * Use an enum for the box color
 // * Implement functionality on the box struct to create a new box
 // * Implement functionality on the box struct to print the characteristics
-
-fn main() {}
+enum Color {
+    Red,
+    Blue,
+    Yellow
+}
+struct Dimension {
+    width: f64,
+    heigt: f64,
+    length: f64
+}
+struct Shippingbox {
+    color: Color,
+    weight: f64,
+    dimension: Dimension,
+}
+impl Shippingbox {
+    fn show_box(&self) {
+        println!("{} Color of the box ", self.color);
+        println!("{} weight of the box", self.weight );
+        }
+    }
+fn main() {
+    let box_temp = Shippingbox {
+    color: Color::Blue,
+    weight: 100.0,
+    dimension: Dimension { width: (10.0), heigt: (20.0), length: (40.0) }
+    };
+    box_temp.show_box();
+}
