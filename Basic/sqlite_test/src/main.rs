76804930eb092dc8cf452a6e2d7538fc9fca8bba
Martin1088 <martin.jurk@dkfz-heidelberg.de>
2023-08-29 07:03:52 +0000

    sqlite3 test problem

--- Basic/sqlite_test/src/main.rs
+++ Basic/sqlite_test/src/main.rs
@@ -1,4 +1,5 @@
-use rusqlite;
+//use rusqlite;
+
 fn table_create() -> rusqlite::Result<()>{
     let conn = rusqlite::Connection::open("dogs.db")?;
     conn.execute(
@@ -7,11 +8,17 @@ fn table_create() -> rusqlite::Result<()>{
         name varchar(50),
         place varchar(50)
             )",
-            (),
+           (),
             )?;
     Ok(())
 }
 
-fn main() {
+fn print_type_of<T>(_:&T) {
+    println!("Type: {:?}", std::any::type_name::<T>())
+}
 
+
+fn main() {
+    let res = table_create();
+    print_type_of(&res);
 }
