eb2c2e8d2bb05bf7f88b8f9151f34595293fb501
Martin Jurk <martin.jurk@dkfz-heidelberg.de>
2023-12-05 16:22:21 +0000

    json, soap

--- Basic/axum-workspace/soap/src/main.rs
+++ Basic/axum-workspace/soap/src/main.rs
@@ -1,35 +1,17 @@
-use std::net::SocketAddr;
-
-use axum::body;
-use axum::http::{Request, Response, StatusCode};
+mod route;
 use axum::routing::get;
 use axum::routing::Router;
-
-async fn soap_request_handler() -> Result<Response<T>, StatusCode> {
-    // Simulate handling the SOAP request
-    // Here, you'd parse the SOAP request and perform the required operations
-
-    // Simulate a SOAP response
-    let soap_response = r#"
-        <SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/">
-            <SOAP-ENV:Body>
-                <Response>Data from SOAP request</Response>
-            </SOAP-ENV:Body>
-        </SOAP-ENV:Envelope>
-    "#;
-
-    let response = Response::builder()
-        .status(StatusCode::OK)
-        .header("Content-Type", "text/xml")
-        .body(soap_response.into())
-        .unwrap();
-    response.text().unwrap()
-}
+use route::{json_test, plain_text, soap_test};
+use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
     // Create a new Axum router
-    let app = Router::new().route("/soap", get(soap_request_handler));
+    let app = Router::new()
+        .route("/soap", get(soap_test))
+        .route("/plain_text", get(plain_text))
+        .route("/json", get(json_test));
+
     let addr = SocketAddr::from(([0, 0, 0, 0], 8000));
     // Run the server on port 8000
     axum::Server::bind(&addr)
