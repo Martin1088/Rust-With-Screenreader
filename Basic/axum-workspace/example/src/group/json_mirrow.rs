f23a7f03d6056756bd9e97fc9ec1bc1a55737044
Martin Jurk <martin.jurk@dkfz-heidelberg.de>
2023-12-09 18:58:30 +0000

    serde json

--- Basic/axum-workspace/example/src/group/json_mirrow.rs
+++ Basic/axum-workspace/example/src/group/json_mirrow.rs
@@ -1,11 +1,15 @@
-use serde
+use axum::Json;
+use serde::Serialize;
+
 struct Room {
     name: String,
     room: String,
 }
-struct Area{
+struct Area {
     mrbs_area: String,
-    room: Vec<Room>
+    room: Vec<Room>,
 }
 
-pub async json_mirrow() {}
+pub async fn json_mrbs() {}
+
+pub async fn json_mirrow() {}
